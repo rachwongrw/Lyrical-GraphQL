f3295f9f3a30d604dd9879db409be8e43f238eb2
Rachel Wong <rwrachelwong@gmail.com>
2022-10-20 08:56:06 -0700

    change to class component, move query to another file

--- client/components/SongCreate.js
+++ client/components/SongCreate.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import gql from "graphql-tag";
 import { graphql } from "react-apollo";
+import { Link, hashHistory } from "react-router";
+import fetchSongs from "../queries/fetchSongs";
 class SongCreate extends Component {
   constructor(props) {
     super(props);
@@ -12,16 +14,22 @@ class SongCreate extends Component {
   onSubmit(event) {
     event.preventDefault();
 
-    this.props.mutate({
-      variables: {
-        title: this.state.title,
-      },
-    });
+    this.props
+      .mutate({
+        variables: {
+          title: this.state.title,
+        },
+        refetchQueries: [{ query: fetchSongs }],
+      })
+      .then(() => {
+        hashHistory.push("/");
+      });
   }
 
   render() {
     return (
       <div>
+        <Link to="/">Back</Link>
         <h3>Create a new song</h3>
         <form onSubmit={this.onSubmit.bind(this)}>
           <label>Song Title:</label>
--- client/components/SongList.js
+++ client/components/SongList.js
@@ -1,33 +1,38 @@
-import React from "react";
-import gql from "graphql-tag";
+import React, { Component } from "react";
 import { graphql } from "react-apollo";
+import { Link } from "react-router";
+import fetchSongs from "../queries/fetchSongs";
 
-const SongList = (props) => {
-  const { songs, loading } = props.data;
-  if (loading) {
-    return <div>Loading...</div>;
-  }
+class SongList extends Component {
+  renderSongs() {
+    const { songs } = this.props.data;
 
-  return (
-    <div className="collection">
-      {songs.map((song) => (
+    if (songs) {
+      return songs.map((song) => (
         <li key={song.id} className="collection-item">
           {song.title}
         </li>
-      ))}
-    </div>
-  );
-};
-
-const query = gql`
-  {
-    songs {
-      id
-      title
+      ));
     }
+    return <div>no songs</div>;
   }
-`;
 
-export default graphql(query)(SongList);
+  render() {
+    if (this.props.loading) {
+      return <div>...Loading...</div>;
+    }
+
+    return (
+      <div>
+        <ul className="collection">{this.renderSongs()}</ul>
+        <Link to="/songs/new" className="btn-floating btn-large red right">
+          <i className="material-icons">add</i>
+        </Link>
+      </div>
+    );
+  }
+}
+
+export default graphql(fetchSongs)(SongList);
 // similar syntax to Redux. graphql(query) returns a fn which is immediately called by the second parentheses, (SongList).
 // The query is rendered when the component is rendered. Once query is complete, the component is rerendered
